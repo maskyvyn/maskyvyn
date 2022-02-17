3bf66ca40895dab57fb4ae2caf8c6fb1926695ce
Neoxxic <makyvinarao@gmail.com>
2022-02-17 13:52:56 +0800

    First Commit

---
+++ footer.php
@@ -0,0 +1,92 @@
+<footer class="footer_wrapper" id="contact">
+		<div class="container">
+			<section class="page_section contact" id="contact">
+				<div class="contact_section">
+					<h2>Get In Touch</h2>
+					<h6>Lorem ipsum dolor sit amet, consectetur Morbi sagittis, sem quisci ipsum</h6>
+
+				</div>
+				<div class="row">
+					<div class="col-lg-4 wow fadeInLeft">
+						<div class="contact_info">
+							<div class="detail">
+								<h4>Johnatan</h4>
+								<p>104, Some street, NewYork, USA</p>
+							</div>
+							<div class="detail">
+								<h4>call us</h4>
+								<p>+1 234 567890</p>
+							</div>
+							<div class="detail">
+								<h4>Email us</h4>
+								<p>support@sitename.com</p>
+							</div>
+						</div>
+
+
+
+						<ul class="social_links">
+							<li class="twitter animated bounceIn wow delay-02s"><a href="javascript:void(0)"><i
+										class="fa fa-twitter"></i></a></li>
+							<li class="facebook animated bounceIn wow delay-03s"><a href="javascript:void(0)"><i
+										class="fa fa-facebook"></i></a></li>
+							<li class="pinterest animated bounceIn wow delay-04s"><a href="javascript:void(0)"><i
+										class="fa fa-pinterest"></i></a></li>
+							<li class="gplus animated bounceIn wow delay-05s"><a href="javascript:void(0)"><i
+										class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-lg-8 wow fadeInLeft delay-06s">
+						<!--NOTE: Update your email Id in "contact_me.php" file in order to receive emails from your contact form-->
+
+						<form name="sentMessage" id="contactForm" novalidate>
+							<div class="control-group">
+								<div class="controls">
+									<input type="text" class="form-control" placeholder="Full Name" id="name" required
+										data-validation-required-message="Please enter your name" />
+									<p class="help-block"></p>
+								</div>
+							</div>
+							<div class="control-group">
+								<div class="controls">
+									<input type="email" class="form-control" placeholder="Email" id="email" required
+										data-validation-required-message="Please enter your email" />
+								</div>
+							</div>
+
+							<div class="control-group">
+								<div class="controls">
+									<textarea rows="10" cols="100" class="form-control" placeholder="Message"
+										id="message" required
+										data-validation-required-message="Please enter your message" minlength="5"
+										data-validation-minlength-message="Min 5 characters" maxlength="999"
+										style="resize:none"></textarea>
+								</div>
+							</div>
+							<div id="success"> </div> <!-- For success/fail messages -->
+							<button type="submit" class="btn btn-primary pull-right">Send</button><br />
+						</form>
+					</div>
+				</div>
+			</section>
+		</div>
+		<div class="container">
+			<div class="footer_bottom"><span>Copyright © 2022. Maskyvyn</span> </div>
+		</div>
+	</footer>
+
+	<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
+	<script type="text/javascript" src="js/jquery.nav.js"></script>
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="js/jquery.isotope.js"></script>
+	<script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/wow.js"></script>
+	<script src="contact/jqBootstrapValidation.js"></script>
+	<script src="contact/contact_me.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+
+</html>
